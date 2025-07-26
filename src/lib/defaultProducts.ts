607e1299acc1756dd71a62fd316267e802ccbf0e
Kaiwen Wang <kvnwkw@gmail.com>
2025-07-26 16:46:42 -0400

    change default softwares

--- src/lib/defaultProducts.ts
+++ src/lib/defaultProducts.ts
@@ -13,5 +13,41 @@ export const defaultProducts: Product[] = [
         name: "Darn Tough Socks T4033",
         category: "clothing",
         image: "https://darntough.com/cdn/shop/products/xron3ocgc7ke4jsf8gb7.png?v=1739281899&width=1800"
+    },
+    {
+        id: "3",
+        name: "QBserve",
+        category: "software",
+        image: "https://qotoqot.com/qbserve/img/gallery/overview.png"
+    },
+    {
+        id: "4",
+        name: "Eagle",
+        category: "software",
+        image: "https://eagle.cool/images/home-cta-logo.png"
+    },
+    {
+        id: "5",
+        name: "Zotero",
+        category: "software",
+        image: "https://play-lh.googleusercontent.com/HUH7a2V6jntUvjlCB6wAxkbzuTVzx40RpM6zfhNROZc1ijka4AVdsgvSjnNwULPhNso=w240-h480-rw"
+    },
+    {
+        id: "6",
+        name: "Pixelmator Pro",
+        category: "software",
+        image: "https://3.img-dpreview.com/files/p/E~C208x0S1024x1024T1200x1200~articles/3667956131/Pixelmator_Pro_Icon.jpeg"
+    },
+    {
+        id: "7",
+        name: "Final Cut Pro",
+        category: "software",
+        image: "https://help.apple.com/assets/673BE5C0E115654F7F097772/673BE5C41BAE7922D30F2CE1/en_US/97f5f4dfe6df84d78caacff68ec63538.png"
+    },
+    {
+        id: "8",
+        name: "NetNewsWire",
+        category: "software",
+        image: "https://netnewswire.com/images/nnw-6-icon-256.png"
     }
 ];
